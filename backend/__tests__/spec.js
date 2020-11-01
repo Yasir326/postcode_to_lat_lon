881e27b871dfb3f7cf479a93811fc22c0824949e
Yasir326 <yasirk@nandos.co.uk>
2020-11-01 17:00:59 +0000

    add edge case for getLatlonFromPostcode tests

--- backend/__tests__/spec.js
+++ backend/__tests__/spec.js
@@ -1,8 +1,22 @@
 import { getLatlonFromPostcode } from "../app";
 
 describe("getLatlonFromPostcode", () => {
-    test("it should return latlon for given postcode", async () => {
-        const latlon = await getLatlonFromPostcode("1234");
-        expect(latlon).toEqual("51.5074, 0.1278");
-    })
-})
+  test("it should return latlon for given postcode", async () => {
+    const latlon = await getLatlonFromPostcode("1234");
+    expect(latlon).toEqual("51.5074, 0.1278");
+  });
+});
+
+describe("getLatlonFromPostcode in uppercase", () => {
+  test("it should return latlon for given postcode", async () => {
+    const latlon = await getLatlonFromPostcode("SW161SZ");
+    expect(latlon).toEqual("51.425706, -0.142173");
+  });
+});
+
+describe("getLatlonFromPostcode with spaces between the postcode", () => {
+  test("it should return latlon for given postcode", async () => {
+    const latlon = await getLatlonFromPostcode("SW16 1SZ");
+    expect(latlon).toEqual("51.425706, -0.142173");
+  });
+});
